f2db2e7a33cf2e35fe7c267372ec3ce12831a99e
17303858097 <1523780825@qq.com>
2020-07-10 14:37:33 +0800

    靳前志又新增了内容

--- app/src/main/java/com/jqz/gitproject/MainActivity.java
+++ app/src/main/java/com/jqz/gitproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name="靳前志的项目";
+        String sex="新增内容";
     }
 }
